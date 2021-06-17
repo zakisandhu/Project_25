92d6a9f57e1ccad96a86febcbfde5dfe88b3ac92
Zaki Sandhu <77785571+zakisandhu@users.noreply.github.com>
2021-06-18 01:16:23 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,24 @@
+class Ball{
+    constructor(x,y){
+        var options = {
+            'restitution':0.3,
+            'friction':1.0,
+            'density':1.2
+        }
+        this.body = Bodies.circle(x,y, this.r, options);
+        this.image = loadImage("paper.png")
+        this.r = r
+        World.add(world, this.body)
+    }
+    display(){
+        var pos=this.body.position;		
+        var angle = this.body.angle
+			push()
+			translate(pos.x, pos.y);
+            rotate(angle)
+			imageMode(CENTER)
+			image(this.image,0,0,this.r)
+			pop()
+    }
+
+}
